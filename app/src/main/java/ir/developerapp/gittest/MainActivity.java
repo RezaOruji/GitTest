f403a1a28fccf50045deb8c5ab09ad22d88e2c1c
Reza Oruji <reza.oruji@gmail.com>
2016-08-15 17:55:39 +0430

    Added ClickListener to Button

--- app/src/main/java/ir/developerapp/gittest/MainActivity.java
+++ app/src/main/java/ir/developerapp/gittest/MainActivity.java
@@ -3,19 +3,21 @@ package ir.developerapp.gittest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // added a click listener
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_SHORT).show();
+                //ok
             }
         });
     }
